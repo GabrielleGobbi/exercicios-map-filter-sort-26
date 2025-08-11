0ff3d05de5836f1f5a4e210179c7212fca0363ad
Vyni <vyni.brasil@gmail.com>
2025-08-11 13:58:51 -0300

    Importando o DB para resolver os enunciodos

--- src/test/1-Aproved-return.test.ts
+++ src/test/1-Aproved-return.test.ts
@@ -22,16 +22,4 @@ function approved(students: Student[], approvalGrade: number): string[] {
 const aprovados = approved(classData.students, classData.approvalGrade);
 
 console.log('Alunos Aprovados:', aprovados);
-
-describe('Aproved-returns', () => {
-    test('Fazer uma função que retorne um array de nomes das aprovadas', () => {
-        // Preparação dos dados
-        const entrada: ClassData = classData;
-        const esperado = ['Ashley', 'Sabrina'];
-
-        // Verificação
-        expect(aprovados).toEqual(esperado);
-    });
-});
-
 export { approved, Student, ClassData };
